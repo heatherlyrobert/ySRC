00f7c5261e68f737c0e7d1ca8f01979faa3a0c23
heatherlyrobert <jelloshrike@gmail.com>
2024-07-22 23:42:40 -0700

    2.1u : update for new DEBUG_ macro calls

--- ySRC_priv.h
+++ ySRC_priv.h
@@ -47,8 +47,8 @@
 /*········· ··········· ´·····························´········································*/
 #define     P_VERMAJOR  "2.--, clean, improve, and expand"
 #define     P_VERMINOR  "2.1-, back onto github and moving forward"
-#define     P_VERNUM    "2.1t"
-#define     P_VERTXT    "minor updates to support library changes"
+#define     P_VERNUM    "2.1u"
+#define     P_VERTXT    "update for new DEBUG_ macro calls"
 /*········· ··········· ´·····························´········································*/
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
